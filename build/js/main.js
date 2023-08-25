b0355f168f9b7b08ae8291e1b37d6338e23a0575
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-25 09:06:38 +0530

    lesson 1

---
+++ build/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let username = "SAS";
+console.log(username);
+let a = 10;
+let b = 2;
+let c = 4;
+console.log(a * b);
+console.log(b + c);
+console.log(c - b);
+console.log(b * c);
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+let username = "SAS";
+console.log(username);
+
+let a: number = 10;
+let b = 2;
+let c = 4;
+
+console.log(a * b);
+console.log(b + c);
+console.log(c - b);
+console.log(b * c);

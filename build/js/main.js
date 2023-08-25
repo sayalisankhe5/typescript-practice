4997443e0eafd9f4a5d4f61a8c5cd71c66cc401f
sayalisankhe5 <sayalisankhe96@gmail.com>
2023-08-25 09:59:44 +0530

    lesson 2

--- build/js/main.js
+++ build/js/main.js
@@ -1,10 +1,26 @@
 "use strict";
 let username = "SAS";
 console.log(username);
+let p;
 let a = 10;
 let b = 2;
 let c = 4;
+let someData;
+someData = "Name";
+someData = 42;
+let id;
+id = 10;
+id = "15";
+id = "first";
 console.log(a * b);
 console.log(b + c);
 console.log(c - b);
 console.log(b * c);
+console.log(someData);
+let ans;
+const sum = (a, b) => {
+    //   return a + b;
+    return a + b;
+};
+ans = sum(5, "10");
+console.log("ans", ans);
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
 let username = "SAS";
 console.log(username);
-
+let p;
 let a: number = 10;
 let b = 2;
 let c = 4;
+let someData: any;
+someData = "Name";
+someData = 42;
+let id: string | number;
+id = 10;
+id = "15";
+id = "first";
 
 console.log(a * b);
 console.log(b + c);
 console.log(c - b);
 console.log(b * c);
+console.log(someData);
+let ans: number;
+
+const sum = (a: number, b: string) => {
+  //   return a + b;
+  return a + b;
+};
+
+ans = sum(5, "10");
+console.log("ans", ans);
